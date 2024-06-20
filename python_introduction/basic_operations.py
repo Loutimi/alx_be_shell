a753c5ead94cd6d91d31b9a6d58d0d7fbcd29dd9
loutimi <loutimi59@gmail.com>
2024-06-20 06:44:26 -0700

    Create basic_operations.py inside python_introduction directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+#!/bin/bash
+number1 = 10   #Assigning a value to the variable number1
+number2 = 5    #Assigning a value to the variable number2
+addition = number1 + number2   
+subtraction = number1 - number2
+multiplication = number1 * number2
+print("Addition of ", number1, " and ", number2, " is ", addition)
+print("Subtraction of ", number1, " and ", number2, " is ", subtraction)
+print("Multiplication of ", number1, " and ", number2, " is ", multiplication)
